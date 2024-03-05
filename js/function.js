c768e1e0d8f6666b73bb9d0dac42ab0983acf81f
Anton <example@mail.ru>
2024-03-05 23:12:14 +0300

    Функции возвращаются

--- js/function.js
+++ js/function.js
@@ -76,6 +76,13 @@ const checkMeeting = (startWorkDay = '8:30', endWorkDay = '17:30', startMeeting 
   return false;
 };
 
- console.log(checkMeeting('08:00', '17:30', '14:00', '90')); // true
-
+ //console.log(checkMeeting('08:00', '17:30', '14:00', '90')); // true
+// console.log(checkMeeting('8:0', '10:0', '8:0', 120)); // true
+// console.log(checkMeeting('08:00', '14:30', '14:00', 90)); // false
+// console.log(checkMeeting('14:00', '17:30', '08:0', 90)); // false
+// console.log(checkMeeting('8:00', '17:30', '08:00', 900)); // false
+// console.log(checkMeeting('8:00', '-17:30', 'выа08:00', 900)); // false
+// console.log(checkMeeting('-8:00', '17:30', '08:00', 900)); // false
+// console.log(checkMeeting('8:0', '10:0', '8:0', -120)); // true
+// debugger;
 
