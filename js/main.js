1ebc021d48b15e707b6d6f209fdf1bacf678787b
Anton <example@mail.ru>
2024-03-11 22:29:53 +0300

    7.15. Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import {photoPosts} from './data.js';
 import './util.js';
+import './thumbnails.js';
 console.log(photoPosts);
 
 
---
+++ js/thumbnails.js
@@ -0,0 +1,10 @@
+import {photoPosts} from './data.js';
+const userPicture = document.querySelector('.picture');
+const userPictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+
+
+
+  
